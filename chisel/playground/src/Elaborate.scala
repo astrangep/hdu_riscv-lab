8e865f967ceb294bfe8a2a175d2c0b91a97c5fd1
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-03 12:42:53 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  12:42:53 up 30 min,  0 user,  load average: 0.06, 0.52, 1.16

--- chisel/playground/src/Elaborate.scala
+++ chisel/playground/src/Elaborate.scala
@@ -1,9 +1,8 @@
-import cpu._
+import gcd._
 import circt.stage._
 
 object Elaborate extends App {
-  implicit val cpuConfig = new CpuConfig()
-  def top                = new PuaCpu()
+  def top                = new GCD()
   val generator          = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
--- chisel/playground/src/TestMain.scala
+++
@@ -1,8 +0,0 @@
-import gcd._
-import circt.stage._
-
-object TestMain extends App {
-  def top                = new GCD()
-  val generator          = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-}
