72f2957fdfec9e5a23bdb57de6aa6fcf0daa9481
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-22 23:59:33 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  23:59:33 up 5 min,  0 user,  load average: 0.04, 0.14, 0.08

--- chisel/playground/src/PuaCpu.scala
+++ chisel/playground/src/PuaCpu.scala
@@ -3,27 +3,18 @@ import chisel3.util._
 import cpu._
 import cpu.defines._
 
-// class PuaCpu extends Module {
-//   val io = IO(new Bundle {
-//     val ext_int   = Input(new ExtInterrupt())
-//     val inst_sram = new InstSram()
-//     val data_sram = new DataSram()
-//     val debug     = new DEBUG()
-//   })
-//
-//   val core = Module(new Core())
-//
-//   io.ext_int <> core.io.interrupt
-//   io.inst_sram <> core.io.instSram
-//   io.data_sram <> core.io.dataSram
-//   io.debug <> core.io.debug
-// }
-
 class PuaCpu extends Module {
-  val port = IO(new Bundle {
-    val x = Input(UInt(8.W))
-    val y = Input(UInt(8.W))
-    val s = Output(Bool())
+  val io = IO(new Bundle {
+    val ext_int   = Input(new ExtInterrupt())
+    val inst_sram = new InstSram()
+    val data_sram = new DataSram()
+    val debug     = new DEBUG()
   })
-  port.s:=Mux(port.x>=port.y,1.B,0.B)
+
+  val core = Module(new Core())
+
+  io.ext_int <> core.io.interrupt
+  io.inst_sram <> core.io.instSram
+  io.data_sram <> core.io.dataSram
+  io.debug <> core.io.debug
 }
