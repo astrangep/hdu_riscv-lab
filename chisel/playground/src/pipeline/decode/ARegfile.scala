c2b9c802a3e0f687914b07a4670ba41eadeb651e
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-04-01 19:50:19 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  19:50:19 up  3:12,  0 user,  load average: 0.51, 0.18, 0.12

--- chisel/playground/src/pipeline/decode/ARegfile.scala
+++ chisel/playground/src/pipeline/decode/ARegfile.scala
@@ -29,9 +29,7 @@ class ARegFile extends Module {
   })
 
   // 定义32个XLEN位寄存器
-    val regs = RegInit(VecInit(Seq.tabulate(AREG_NUM)(i => 
-    if (i == 0) 0.U(XLEN.W) else i.U(XLEN.W)
-  )))
+    val regs = RegInit(VecInit(Seq.fill(AREG_NUM)(0.U(XLEN.W))))
   // 写寄存器堆
   when(io.write.wen && io.write.waddr =/= 0.U){
     regs(io.write.waddr) := io.write.wdata
