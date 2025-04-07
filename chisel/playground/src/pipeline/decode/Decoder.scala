271aea7fb65a89e0dcf36ce82b7f19d668a55078
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-04-07 14:55:02 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  14:55:02 up 42 min,  0 user,  load average: 0.31, 0.22, 0.18

--- chisel/playground/src/pipeline/decode/Decoder.scala
+++ chisel/playground/src/pipeline/decode/Decoder.scala
@@ -30,6 +30,7 @@ class Decoder extends Module with HasInstrType {
   io.out.info.src1_raddr := rs
   io.out.info.src2_raddr := rt
   io.out.info.op := fuOpType
+  io.out.info.fusel := fuType
   io.out.info.reg_wen := isRegWen(instrType)
   io.out.info.reg_waddr := rd
   io.out.info.is_lui := inst(6, 0) === "b0110111".U
