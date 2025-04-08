92e4afa1b55ad26249fc1ede5c23e279f3bb7253
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-04-08 19:09:16 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  19:09:16 up 33 min,  0 user,  load average: 0.27, 0.24, 0.18

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -49,10 +49,10 @@ class Mdu extends Module {
     MDUOpType.remu   -> rem_unsigned,
     
     // W后缀指令（RV64）
-    MDUOpType.mulw   -> mul_signed(31, 0).sextTo(XLEN),
-    MDUOpType.divw   -> div_signed(31, 0).sextTo(XLEN),
-    MDUOpType.divuw  -> div_unsigned(31, 0).zextTo(XLEN),
-    MDUOpType.remw   -> rem_signed(31, 0).sextTo(XLEN),
-    MDUOpType.remuw  -> rem_unsigned(31, 0).zextTo(XLEN)
+    MDUOpType.mulw   -> SignedExtend(mul_signed(31, 0),XLEN)
+    MDUOpType.divw   -> SignedExtend(div_signed(31, 0),XLEN)
+    MDUOpType.divuw  -> ZeroExtend(div_unsigned(31, 0),XLEN)
+    MDUOpType.remw   -> SignedExtend(rem_signed(31, 0).XLEN)
+    MDUOpType.remuw  -> ZeroExtend(rem_unsigned(31, 0),XLEN)
   ))
 }
