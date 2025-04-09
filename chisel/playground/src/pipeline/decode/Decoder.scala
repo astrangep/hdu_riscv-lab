05a65a3279e4fa40949beef541415b59c92033f1
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-04-09 15:35:12 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  15:35:12 up  1:15,  0 user,  load average: 0.25, 0.11, 0.10

--- chisel/playground/src/pipeline/decode/Decoder.scala
+++ chisel/playground/src/pipeline/decode/Decoder.scala
@@ -23,6 +23,7 @@ class Decoder extends Module with HasInstrType {
     ListLookup(inst, Instructions.DecodeDefault, Instructions.DecodeTable)
   val imm_i = SignedExtend(inst(31, 20), 64)
   val imm_u = SignedExtend(Cat(inst(31, 12), 0.U(12.W)), 64)
+  val imm_s = SignedExtend(Cat(inst(31, 25), inst(11, 7)), 64)
 
   val (rs, rt, rd) = (inst(19, 15), inst(24, 20), inst(11, 7))
   io.out.info.inst := inst
@@ -34,7 +35,7 @@ class Decoder extends Module with HasInstrType {
   io.out.info.reg_wen := isRegWen(instrType)
   io.out.info.reg_waddr := rd
   io.out.info.is_lui := inst(6, 0) === "b0110111".U
-  io.out.info.imm := MuxLookup(instrType, 0.U)(Seq(InstrI -> imm_i, InstrU -> imm_u))
-  io.out.info.src1_ren := instrType === InstrR | instrType === InstrI
-  io.out.info.src2_ren := instrType === InstrR
+  io.out.info.imm := MuxLookup(instrType, 0.U)(Seq(InstrI -> imm_i, InstrU -> imm_u, InstrS -> imm_s))
+  io.out.info.src1_ren := instrType === InstrR | instrType === InstrI | instrType === InstrS
+  io.out.info.src2_ren := instrType === InstrR | instrType === InstrS
 }
