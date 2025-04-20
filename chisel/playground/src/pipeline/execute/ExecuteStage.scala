e677af06d8fbde3ea92caf30f68b6236805e080f
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-04-20 18:32:42 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  18:32:42 up  3:48,  0 user,  load average: 0.28, 0.35, 0.31

--- chisel/playground/src/pipeline/execute/ExecuteStage.scala
+++ chisel/playground/src/pipeline/execute/ExecuteStage.scala
@@ -27,6 +27,7 @@ class ExecuteStage extends Module {
 
   data.info := io.decodeUnit.data.info
   data.pc := io.decodeUnit.data.pc
+  data.src_info := io.decodeUnit.data.src_info
   data.info.valid := Mux(io.decodeUnit_ctrl.do_flush, false.B, io.decodeUnit.data.info.valid && io.decodeUnit_ctrl.allow_to_go)
   io.executeUnit.data := data
 }
