88f529b6f7710b40caa3512c5c0e2dd69c42da87
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-04-20 23:45:30 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  23:45:30 up  9:01,  0 user,  load average: 0.11, 0.13, 0.13

--- chisel/playground/src/pipeline/decode/DecodeStage.scala
+++ chisel/playground/src/pipeline/decode/DecodeStage.scala
@@ -25,6 +25,6 @@ class DecodeStage extends Module {
 
   val data = RegInit(0.U.asTypeOf(new IfIdData()))
   data := Mux(io.fetchUnit_ctrl.do_flush, 0.U.asTypeOf(new IfIdData()), Mux(io.fetchUnit_ctrl.allow_to_go, io.fetchUnit.data, data))
-  data.valid := !io.fetchUnit_ctrl.do_flush && io.fetchUnit.data.valid && io.fetchUnit_ctrl.allow_to_go
   io.decodeUnit.data := data
+  io.decodeUnit.data.valid := !io.fetchUnit_ctrl.do_flush && io.fetchUnit.data.valid && io.fetchUnit_ctrl.allow_to_go
 }
--- chisel/playground/src/pipeline/memory/MemoryStage.scala
+++ chisel/playground/src/pipeline/memory/MemoryStage.scala
@@ -26,6 +26,6 @@ class MemoryStage extends Module {
 
   val data = RegInit(0.U.asTypeOf(new ExeMemData()))
   data := Mux(io.executeUnit_ctrl.do_flush, 0.U.asTypeOf(new ExeMemData()), Mux(io.executeUnit_ctrl.allow_to_go, io.executeUnit.data, data))
-  data.info.valid := !io.executeUnit_ctrl.do_flush && io.executeUnit.data.info.valid && io.executeUnit_ctrl.allow_to_go
   io.memoryUnit.data := data
+  io.memoryUnit.data.info.valid := !io.executeUnit_ctrl.do_flush && io.executeUnit.data.info.valid && io.executeUnit_ctrl.allow_to_go
 }
