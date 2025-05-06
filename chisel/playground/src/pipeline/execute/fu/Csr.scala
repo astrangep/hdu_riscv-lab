314061b1d89b69d22bbe323f19b3a35de2454c6d
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-06 22:02:10 +0800

    > generate verilog 244050090 Felix Linux predatorstart 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  22:02:10 up  1:12,  0 user,  load average: 1.58, 0.64, 0.47

--- chisel/playground/src/pipeline/execute/fu/Csr.scala
+++ chisel/playground/src/pipeline/execute/fu/Csr.scala
@@ -75,7 +75,7 @@ class Csr extends Module {
     ),
     CSR_ADDRS("MCOUNTEREN") -> CsrReg(
       reg = RegInit(0.U(XLEN.W)),
-      writeMask = "hfffffffffffffff7".U(XLEN.W),
+      writeMask = "hffffffffffffffff".U(XLEN.W),
       write = true.B
     ),
     CSR_ADDRS("MSCRATCH") -> CsrReg(
